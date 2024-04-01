4a174c637e59802b19008a34a153ee3ea747e6d4
Russell-Tran <45905880+Russell-Tran@users.noreply.github.com>
2024-03-31 18:57:56 -0700

    Time: 34 ms (71.62%), Space: 16.6 MB (89.29%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        for char in s:
+            index = t.find(char)
+            if index == -1:
+                return False
+            t = t[index+1:]
+        return True
