e94778f90dcf68aaf4d2e84628b34cc5badea73d
Russell-Tran <45905880+Russell-Tran@users.noreply.github.com>
2024-03-31 18:46:55 -0700

    Time: 51 ms (92.66%), Space: 16.6 MB (79.69%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -35,3 +35,46 @@ class Solution:
         for _ in range(len(chars) - overwrite_index + 1):
             chars.pop()
         return overwrite_index
+
+    
+    """
+    
+    class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if len(chars) < 2:
+            return len(chars)
+        
+        chars.append("")
+        overwrite_index = 1
+        current_count = 1
+        for i in range(1, len(chars)):
+            if chars[i] == chars[i-1]:
+                current_count += 1
+            else:                    
+                if 1 < current_count and current_count < 10:
+                    chars[overwrite_index] = str(current_count)
+                    overwrite_index += 1
+                else: # current count >= 10
+                    
+                    # need to do maneuver to store the digits, 
+                    # go back to front s.t. most significant digit ends up first
+                    digits = 0
+                    ct = current_count
+                    while ct > 0:
+                        digits += 1
+                        ct //= 10
+                    ct = current_count
+                    back = 1
+                    while ct > 0:
+                        char[overwrite_index + digits - back] = str(ct % 10)
+                        ct //= 10
+                        back += 1
+                    overwrite_index += digits
+                    
+                chars[overwrite_index] = chars[i]
+                overwrite_index += 1
+                current_count = 1
+                
+        chars.pop()
+        return overwrite_index
+    """
