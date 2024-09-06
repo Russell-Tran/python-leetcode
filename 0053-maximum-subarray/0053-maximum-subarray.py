0d63adfb81b4385309a1f278d8b0c28e9df628fb
Russell-Tran <45905880+Russell-Tran@users.noreply.github.com>
2024-09-05 18:23:42 -0700

    Time: 557 ms (21.80%), Space: 31.1 MB (14.42%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,14 +4,14 @@ class Solution:
             return nums[0]
         
         
-        def has_positive(nums) -> bool:
-            for num in nums:
-                if num > 0:
-                    return True
-            return False
+#         def has_positive(nums) -> bool:
+#             for num in nums:
+#                 if num > 0:
+#                     return True
+#             return False
         
-        if not has_positive(nums):
-            return max(nums)
+#         if not has_positive(nums):
+#             return max(nums)
         
     
         # keep a running tab
@@ -23,7 +23,7 @@ class Solution:
         # then ask yourself whether you come out ahead
         # if you don't come out ahead, then cut off your tail and start anew
         
-        best_summation = nums[0]
+        best_summation = max(nums)
         current_summation = nums[0]
         
         for i in range(1, len(nums)):
