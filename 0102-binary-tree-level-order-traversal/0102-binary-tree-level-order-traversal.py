0a428eb7d672913dcc2733ecdaebafe64341336d
Russell-Tran <45905880+Russell-Tran@users.noreply.github.com>
2025-05-13 22:09:53 -0700

    Time: 4 ms (7.43%), Space: 18.5 MB (60.28%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+
+        """
+        Initial thoughts:
+        This looks like BFS - BFS works well with a queue
+        """
+        if not root:
+            return []
+
+        result = []
+        package = []
+        queue = deque([root, None]) # None is a boundary marker here
+        while queue:
+            node = queue.popleft()
+            if node is None: # create the beautiful little packages
+                result.append(package)
+                package = []
+                continue
+
+            package.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            if len(queue) > 1 and queue[0] is None:
+                queue.append(None) # add back a marker only if we've finished the level
+
+        return result
+
+        
+        
