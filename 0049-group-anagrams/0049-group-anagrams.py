243185e78534236d2270a08048bf1b20a0396fd6
Russell-Tran <45905880+Russell-Tran@users.noreply.github.com>
2025-06-03 13:35:33 -0700

    Time: 9 ms (90.24%), Space: 20.8 MB (62.87%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,18 @@
 class Solution:
+    # O(N K log K)
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         families = {}
-        for s in strs:
-            family_id = "".join(sorted(s))
-            if family_id not in families:
+        for s in strs: # O(N)
+            family_id = "".join(sorted(s)) # O(K log K) to sort a string
+            if family_id not in families: # O(1) hash lookup
                 families[family_id] = []
             families[family_id].append(s)
-        return list(families.values())
+        return list(families.values()) # O(N) data copy
 
 """
 - a sorted string allows all anagrams to match
 - use dictionary of the sorted string -> list of anagrams
 - return the values of the dictionary as a list
-
+- NOTE: needs "".join() to turn a list of char into a string, or just tuplify the list for hashing
 
 """
