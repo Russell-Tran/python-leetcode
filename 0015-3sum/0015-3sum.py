2ddc39b4a60e07e5c9a847341e3a2baeae5244d6
Russell-Tran <45905880+Russell-Tran@users.noreply.github.com>
2025-06-07 19:11:16 -0700

    Time: 1075 ms (16.29%), Space: 21.9 MB (7.08%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,6 +5,7 @@ class Solution:
         #print("nums = {}".format(nums))
         for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i-1]:
+                # We do this because the point of i is to be unique
                 continue
             j, k = i + 1, len(nums) - 1
             while j < k:
@@ -16,6 +17,14 @@ class Solution:
                 # if k < len(nums) - 2 and nums[k] == nums[k+1] == nums[k+2]:
                 #     k -= 1
                 #     continue
+                # if j - i > 2 and nums[j] == nums[j-i] and k - j > 1:
+                #     j += 1
+                #     continue
+
+                if k < len(nums) - 1 and nums[k] == nums[k+1]:
+                    k -= 1
+                    continue
+
                 summation = nums[i] + nums[j] + nums[k]
                 if summation == 0:
                     #result.append([nums[i], nums[j], nums[k]])
