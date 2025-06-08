768c95c5d4a8948bc60f4015b3ce5824fc2aa18f
Russell-Tran <45905880+Russell-Tran@users.noreply.github.com>
2025-06-07 17:50:16 -0700

    Time: 4 ms (48.37%), Space: 18.8 MB (9.54%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,21 +1,17 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        def format(i, j):
-            return [i+1, j+1]
-
-        for i, number in enumerate(numbers):
-            counterpart = target - number
-            low = i + 1
-            high = len(numbers) - 1
-            while low <= high:
-                mid = (low + high) // 2
-                if numbers[mid] == counterpart:
-                    return format(i, mid)
-                elif numbers[mid] < counterpart:
-                    low = mid + 1
-                else:
-                    high = mid - 1
+        low = 0
+        high = len(numbers) - 1
+        while low < high:
+            if numbers[low] + numbers[high] == target:
+                return [low + 1, high + 1]
+            elif numbers[low] + numbers[high] > target:
+                high -= 1
+            else:
+                low += 1
 """
+ANKI
+
 NOTES
 already sorted in non-decreasing order
 
@@ -39,5 +35,24 @@ everyone else to find the counterpart
 
 O(n log n) solution would be to iterate over the array, and for each, do a binary search
 of the counterpart 
+
+WELL THIS WAS MY BINARY SEARCH SOLN
+which worked but is O(n log n) instead of O(n) so :
+
+        def format(i, j):
+            return [i+1, j+1]
+
+        for i, number in enumerate(numbers):
+            counterpart = target - number
+            low = i + 1
+            high = len(numbers) - 1
+            while low <= high:
+                mid = (low + high) // 2
+                if numbers[mid] == counterpart:
+                    return format(i, mid)
+                elif numbers[mid] < counterpart:
+                    low = mid + 1
+                else:
+                    high = mid - 1
 
 """
