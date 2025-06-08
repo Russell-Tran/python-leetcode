1f4d881b348a814cb4d7cc06c8379de0df3e4cd1
Russell-Tran <45905880+Russell-Tran@users.noreply.github.com>
2025-06-07 17:33:49 -0700

    Time: 43 ms (5.08%), Space: 18.6 MB (59.56%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,18 +1,43 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
-        left_ptr, right_ptr = 0, len(numbers) - 1
-        
-        while left_ptr != right_ptr:
-            left = numbers[left_ptr]
-            right = numbers[right_ptr]
-            summation = right + left
-            
-            if summation == target:
-                break
-            elif summation < target:
-                left_ptr += 1
-            else: # summation > target
-                right_ptr -=1
-        
-        return [left_ptr + 1, right_ptr + 1]
+        def format(i, j):
+            return [i+1, j+1]
+
+        for i, number in enumerate(numbers):
+            counterpart = target - number
+            low = i + 1
+            high = len(numbers) - 1
+            while low <= high:
+                mid = (low + high) // 2
+                if numbers[mid] == counterpart:
+                    return format(i, mid)
+                elif numbers[mid] < counterpart:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+"""
+NOTES
+already sorted in non-decreasing order
+
+find two numbers that add up to a specific target
+
+return the 2 indices 1-indexed 
+
+cannot use same element twice
+
+must use only constant extra space
+
+I think the INSIGHT might be about , since it's in non-decreasing order, 
+you'll know quickly that a counterpart no longer exists 
+
+or the two pointers approach, come from the back to find the counterpart 
+
+well there's also the possibility of binary search 
+
+O(n^2) solution would be to do a nested for-loop and for each element, re-iterate over
+everyone else to find the counterpart 
+
+O(n log n) solution would be to iterate over the array, and for each, do a binary search
+of the counterpart 
+
+"""
